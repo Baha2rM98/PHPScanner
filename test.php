7a2e8464e7b8977f61329c4a12489c4ee7d26aab
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-09-19 16:36:58 +0430

    test.php file updated

--- test.php
+++ test.php
@@ -1,2 +1,27 @@
 <?php
 require_once "vendor\autoload.php";
+
+use Scanner\Scanner;
+
+$scn = new Scanner();
+$a = $scn->nextString();
+print $a;
+
+$n = $scn->nextGMP();
+$m = $scn->nextInt();
+print (gmp_pow($n, $m)) . "\n";
+
+$a = array();
+for ($i = 0; $i < 3; $i++) {
+    $a[$i] = $scn->nextInt();
+}
+print_r($a);
+
+$file_reader = new Scanner("C:\Users\Baha2r\Desktop\a.txt");
+$file = "";
+while ($file_reader->hasNext()) {
+    $file .= $file_reader->nextLine();
+}
+$file_reader->close();
+print $file;
+echo "\n";
