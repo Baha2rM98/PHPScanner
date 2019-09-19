dc11d38e324f78fb8fcba864a03832d9a6067584
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-09-19 13:45:43 +0430

    iterator interface documented

--- src/inputstream/Iterator.php
+++ src/inputstream/Iterator.php
@@ -1,11 +1,26 @@
 <?php
 
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date : 19/9/2019
+ *
+ * This interface is parent iterator to iterate and scan files
+ **/
 
 namespace Iterator;
 
 
 interface Iterator
 {
+    /**
+     * close() will be implemented in child class
+     **/
     public function close();
+
+
+    /**
+     * hasNext() will be implemented in child class
+     **/
     public function hasNext();
 }
