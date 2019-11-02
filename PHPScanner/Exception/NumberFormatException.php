920e2da8eb77f5f2489cfae4e6da96c463d8626c
Baha2r <baha2r.mirzazadeh98@gmail.com>
2019-11-02 17:50:31 +0330

    Number format exception class added

---
+++ PHPScanner/Exception/NumberFormatException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author Baha2r
+ * @license MIT
+ * Date : 02/11/2019
+ *
+ * Number format exception class
+ **/
+
+namespace PHPScanner\Exception;
+
+
+use RuntimeException;
+
+class NumberFormatException extends RuntimeException
+{
+    //
+}
