6c197026ace96ba26d09bdd6a634db15196ae395
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-29 21:37:02 +0330

    Some little changes

--- PHPScanner/Scanner/Scanner.php
+++ PHPScanner/Scanner/Scanner.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Baha2r
+ * @author  Baha2r
  * @license MIT
  * Date : 19/9/2019
  *
@@ -10,98 +10,50 @@
 
 namespace PHPScanner\Scanner;
 
-use Exception;
-//use GMP;
+use PHPScanner\Exception\FileReaderException;
+use PHPScanner\Exception\NumberFormatException;
 use PHPScanner\InputStream\InputStream;
 
 final class Scanner extends InputStream
 {
     // private fields
 
+    /**
+     * @var resource
+     **/
     private $handle;
+
+    /**
+     * @var string
+     **/
     private $line;
+
+    /**
+     * @var boolean
+     **/
     private $flag = false;
+
+    /**
+     * @var string
+     **/
     private const MOD = "r";
 
 
-    //constructor
+    // Constructor
 
     /**
-     * [optional] use this constructor when you want read a file through Scanner class
+     * [optional] Use this constructor when you want read a file through Scanner class
      * <p>Initiate handle of the file (resource)</p>
-     * @param string $file gets a file path to scan and read '</br>'
+     *
+     * @param  string  $file  Gets a file path to scan and read '</br>'
      **/
     public function __construct($file = null)
     {
+        parent::__construct();
         if (!is_null($file)) {
             $this->handle = fopen($file, self::MOD);
             $this->flag = true;
         }
-    }
-
-
-    /**
-     * @param null $from [optional] change base of entered number from $from
-     * @param int $radix [optional] change base of entered number to $radix
-     * @return integer return entered integer
-     * @throws Exception
-     */
-    public function nextInt($from = null, $radix = null)
-    {
-        $val = $this->createNewInput();
-        if (is_null($from) && is_null($radix))
-            return $this->toInt($val);
-        return $this->toInt($this->changeRadix($val, $from, $radix));
-    }
-
-
-    /**
-     * @param int $radix [optional] change base of entered number to $radix
-     * @param null $from [optional] change base of entered number from $from
-     * @return float return entered float
-     * @throws Exception
-     */
-    public function nextFloat($from = null, $radix = null)
-    {
-        $val = $this->createNewInput();
-        if (is_null($from) && is_null($radix))
-            return $this->toFloat($val);
-        return $this->toFloat($this->changeRadix($val, $from, $radix));
-    }
-
-
-//    /**
-//     * @param null $from [optional] change base of entered number from $from
-//     * @param int $radix [optional] change base of entered number to $radix
-//     * @return GMP return entered GMP number
-//     * @throws Exception
-//     */
-//    public function nextGMP($from = null, $radix = null)
-//    {
-//        $val = $this->createNewInput();
-//        if (is_null($from) && is_null($radix))
-//            return $this->toGMP($val);
-//        return $this->toGMP($this->changeRadix(gmp_strval($val), $from, $radix));
-//    }
-
-
-    /**
-     * @return string return entered character
-     **/
-    public function nextChar()
-    {
-        $val = $this->createNewInput();
-        return $this->toChar($val);
-    }
-
-
-    /**
-     * @return string return entered string
-     **/
-    public function nextString()
-    {
-        $val = $this->createNewInput();
-        return $this->toString($val);
     }
 
 
@@ -117,10 +69,11 @@ final class Scanner extends InputStream
 
 
     /**
-     * @param string $number
-     * @param integer $to
-     * @param integer $from
-     * @return string return converted number from base $from to base $to
+     * @param  string   $number
+     * @param  integer  $to
+     * @param  integer  $from
+     *
+     * @return string Returns converted number from base $from to base $to
      */
     private function changeRadix($number, $from, $to)
     {
@@ -129,45 +82,54 @@ final class Scanner extends InputStream
 
 
     /**
-     * @param string $value
-     * @return integer return converted $value to integer
-     * @throws Exception
+     * @param  string  $value
+     *
+     * @return integer Returns converted $value to integer
+     * @throws NumberFormatException
      */
     private function toInt($value)
     {
-        if (preg_match("/[a-z]/i", $value))
-            throw new Exception("Number Format Exception!");
+        if (!preg_match('/^[0-9a-fA-F]+$/', $value)) {
+            throw new NumberFormatException("Number Format Exception!");
+        }
         return intval($value);
     }
 
 
     /**
-     * @param string $value
-     * @return float return converted $value to float
-     * @throws Exception
+     * @param  string  $value
+     *
+     * @return float Returns converted $value to float
+     * @throws NumberFormatException
      */
     private function toFloat($value)
     {
-        if (preg_match("/[a-z]/i", $value))
-            throw new Exception("Number Format Exception!");
+        if (!preg_match('/^[0-9a-fA-F.]+$/', $value)) {
+            throw new NumberFormatException("Number Format Exception!");
+        }
         return floatval($value);
     }
 
 
 //    /**
-//     * @param string $value
-//     * @return GMP return converted $value to GMP number
-//     * @throws Exception
-//     **/
-//    private function toGMP($value)
+//     * @param  string   $value
+//     * @param  integer  $scale
+//     *
+//     * @return string Returns converted $value to string number
+//     */
+//    private function toBigNumber($value, $scale)
 //    {
-//        return gmp_init($value);
+//        if (!preg_match('/^[0-9.]+$/', $value)) {
+//            throw new NumberFormatException("Number Format Exception!");
+//        }
+//        return bcadd($value, "0", $scale);
 //    }
 
 
     /**
-     * @param string $value
-     * @return string return converted $value to string
+     * @param  mixed  $value
+     *
+     * @return string Returns converted $value to string
      **/
     private function toString($value)
     {
@@ -176,52 +138,142 @@ final class Scanner extends InputStream
 
 
     /**
-     * @param string $value
-     * @return string return converted $value to string(character)
+     * @param  string  $value
+     *
+     * @return string Returns converted $value to string(character)
      **/
     private function toChar($value)
     {
         $str = str_split($value);
-        return $str[0];
+        $char = $str[0];
+        unset($str);
+        return $char;
+    }
+
+
+    /**
+     * Returns entered integer number
+     *
+     * @param  integer  $from   [optional] change base of entered number from $from
+     * @param  integer  $radix  [optional] change base of entered number to $radix
+     *
+     * @return integer|string Returns entered integer
+     * @throws NumberFormatException
+     */
+    public function nextInt($from = null, $radix = null)
+    {
+        $val = $this->createNewInput();
+        if (is_null($from) || is_null($radix)) {
+            return $this->toInt($val);
+        }
+        return strval($this->changeRadix($val, $from, $radix));
+    }
+
+
+    /**
+     * Returns entered float or double number
+     *
+     * @param  integer  $radix  [optional] change base of entered number to $radix
+     * @param  integer  $from   [optional] change base of entered number from $from
+     *
+     * @return float|string Returns entered float
+     * @throws NumberFormatException
+     */
+    public function nextFloat($from = null, $radix = null)
+    {
+        $val = $this->createNewInput();
+        if (is_null($from) || is_null($radix)) {
+            return $this->toFloat($val);
+        }
+        return strval($this->changeRadix($val, $from, $radix));
+    }
+
+
+//    /**
+//     * @param  null  $from   [optional] change base of entered number from $from
+//     * @param  int   $radix  [optional] change base of entered number to $radix
+//     *
+//     * @param  int   $scale
+//     *
+//     * @return string Returns entered BigNumber
+//     */
+//    public function nextBigNumber($from = null, $radix = null, $scale = 5)
+//    {
+//        $val = $this->createNewInput();
+//        if (is_null($from) || is_null($radix)) {
+//            return $this->toBigNumber($val, $scale);
+//        }
+//        return $this->changeRadix(bcadd(strval($val), "0", $scale), $from, $radix);
+//    }
+
+
+    /**
+     * Returns entered string's first character
+     *
+     * @return string Returns entered character
+     **/
+    public function nextChar()
+    {
+        $val = $this->createNewInput();
+        return $this->toChar($val);
+    }
+
+
+    /**
+     * Returns entered string
+     *
+     * @return string Returns entered string
+     **/
+    public function nextString()
+    {
+        $val = $this->createNewInput();
+        return $this->toString($val);
     }
 
 
     // files methods
 
     /**
-     * @return string return content of file
-     * @throws Exception
+     * Returns content of the file
+     *
+     * @return string Returns content of file
+     * @throws FileReaderException
      */
     public function nextLine()
     {
-        if (!$this->flag)
-            throw new Exception("Constructor Must Be Initialize With A File's Path!");
+        if (!$this->flag) {
+            throw new FileReaderException("Constructor Must Be Initialize With A File's Path!");
+        }
         $content = $this->line;
         return $content;
     }
 
 
     /**
-     * @return string|boolean read file line by line and return content as string, return false if end of file achieved
-     * @throws Exception
+     * Returns next line of the file, False otherwise
+     *
+     * @return string|boolean Returns lines of the file content as string, return false if end of file achieved
+     * @throws FileReaderException
      */
     public function hasNext()
     {
-        if (!$this->flag)
-            throw new Exception("Constructor Must Be Initialize With A File's Path!");
+        if (!$this->flag) {
+            throw new FileReaderException("Constructor Must Be Initialize With A File's Path!");
+        }
         return (($this->line = fgets($this->getHandle())) !== false);
     }
 
 
     /**
-     * Close opened handle
-     * @return void
-     * @throws Exception
+     * Closes opened handle
+     *
+     * @throws FileReaderException
      */
-    public function close()
+    public function close(): void
     {
-        if (!$this->flag)
-            throw new Exception("Constructor Must Be Initialize With A File's Path!");
+        if (!$this->flag) {
+            throw new FileReaderException("Constructor Must Be Initialize With A File's Path!");
+        }
         fclose($this->getHandle());
     }
 }
