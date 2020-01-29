546abdfbe36face3191b9c2fad0916d63381defa
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-29 18:57:31 +0330

    FileReadeException class added

---
+++ PHPScanner/Exception/FileReaderException.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author  Baha2r
+ * @license MIT
+ * Date : 29/01/2020
+ *
+ * File reade exception class
+ **/
+
+namespace PHPScanner\Exception;
+
+
+use RuntimeException;
+
+class FileReaderException extends RuntimeException
+{
+    //
+}
