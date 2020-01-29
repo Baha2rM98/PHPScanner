4af9185950cf544c07377f53145e7435b0b7ab3a
Baha2rM98 <baha2r.mirzazadeh98@gmail.com>
2020-01-29 19:08:31 +0330

    Constructor added

--- PHPScanner/InputStream/InputStream.php
+++ PHPScanner/InputStream/InputStream.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * @author Baha2r
+ * @author  Baha2r
  * @license MIT
  * Date : 19/9/2019
  *
@@ -16,9 +16,16 @@ abstract class InputStream implements Iterator, Closeable
     // private fields
     private const in = STDIN;
 
+    // Constructor
+    protected function __construct()
+    {
+        //
+    }
+
 
     /**
      * This method creates a new input stream data
+     *
      * @return string|boolean return input stream data
      **/
     protected final function createNewInput()
